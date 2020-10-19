c0c86b9a4dee1532e07084ea7dc3eb152f8bb808
David Wisecarver <dwisecar@gmail.com>
2020-10-19 02:52:12 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,3 +8,29 @@ def loop_message_five_times(string)
   end
 end
 
+def loop_message_n_times(string, int)
+    counter = 0
+  while counter < int do
+    puts string
+    counter += 1
+  end
+end
+
+def output_array(array)
+    counter = 0
+    while counter < array.length do
+    puts array[counter]
+    counter += 1
+  end
+end
+
+def return_string_array(array)
+  new_array = []
+  counter = 0
+  while counter < array.length do
+    new_array[counter] = array[counter].to_s
+    counter += 1
+  end
+  new_array
+end
+
